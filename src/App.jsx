bec0f0c2df6f8ece1651a57e7c5984e696683b1d
devbit01 <devbit01@gmail.com>
2023-02-25 15:31:54 -0600

    Add modal.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from 'react'
 
 function App() {
-    return <div className="App"></div>
+    const [modalOpen, setModalOpen] = useState(false)
+    return (
+        <div className="App">
+            <button onClick={() => setModalOpen(true)}>Click Me</button>
+            {modalOpen && (
+                <div className="modal-bg" onClick={() => setModalOpen(false)}>
+                    <div className="modal">Click to close</div>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default App
